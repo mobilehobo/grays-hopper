df988a1310efb084fc5cea9de6394fb8c65df4c6
David Geiss <dave.geiss@gmail.com>
2017-07-28 16:26:58 -0400

    beers tests

--- server/beer.test.js
+++ server/beer.test.js
@@ -1,4 +1,4 @@
-/* global describe it before afterEach beforeEach */
+/* global describe it before afterEach beforeEach xit xdescribe */
 
 const request = require('supertest-as-promised'),
 	{ expect } = require('chai'),
@@ -21,12 +21,13 @@ describe('Beer routes', () => {
 	let beer1, beer2;
 	let parentCompany;
 	let tag1, tag2;
+
 	beforeEach(() =>
 		ParentCompany.create({
 			name: 'Busch'
-		}).then(result => {
-			parentCompany = result;
 		})
+			.then(result =>
+				parentCompany = result)
 			.then(() =>
 				Tag.create({
 					name: 'Crisp'
@@ -65,28 +66,77 @@ describe('Beer routes', () => {
 					parent_company_id: parentCompany.id
 				}))
 			.then(result => beer2 = result)
-		// .then(() =>
-		// 	BeerTag.create({
-		// 		beer_id: beer1.id,
-		// 		tag_id: tag2.id
-		// 	}))
-		// .then(() =>
-		// 	BeerTag.create({
-		// 		beer_id: beer2.id,
-		// 		tag_id: tag1.id
-		// 	}));
+			.then(() =>
+				BeerTag.create({
+					beer_id: beer1.id,
+					tag_id: tag2.id
+				}))
+			.then(() =>
+				BeerTag.create({
+					beer_id: beer2.id,
+					tag_id: tag1.id
+				}))
 	);
 
-	// afterEach('Clear the tables', () => db.truncate({ cascade: true }));
+	afterEach('Clear the tables', () => db.truncate({ cascade: true }));
 
 	describe('GET routes', () => {
 		it('should get all beers when going to /api/beers', () =>
 			agent.get('/api/beers')
 				.expect(200)
 				.expect(res => {
-					console.log(res.body);
 					expect(res.body).to.be.an.instanceOf(Array);
 					expect(res.body).to.have.length(2);
+				}));
+
+		it('should include the parent companies', () =>
+			agent.get('/api/beers')
+				.expect(200)
+				.expect(res => {
+					for (let beer in res.body) {
+						expect(res.body[beer].parentCompany.name).to.not.be.equal(undefined);
+					}
+				}));
+
+		it('should include the relevant tags', () =>
+			agent.get('/api/beers')
+				.expect(200)
+				.expect(res => {
+					for (let beer in res.body) {
+						expect(res.body[beer].tags).to.be.an.instanceOf(Array);
+						expect(res.body[beer].tags).to.be.length(1);
+						expect(res.body[beer].tags[0]).to.not.be.equal(undefined);
+					}
+				}));
+
+		xit('should get a single beer when going to /api/beer/:id', () =>
+			agent.get(`/api/beers/${beer1.id}`)
+				.expect(200)
+				.expect(res => {
+					expect(res.body).to.be.an.instanceOf(Object);
+					expect(res.body.name).to.be.equal(beer1.name);
+				}));
+	});
+
+	describe('POST route', () => {
+		it('should make a new beer when posting to /api/beer/', () =>
+			agent.post('/api/beers')
+				.send({
+					name: 'Fransiskaner',
+					price: 6,
+					description: 'Delicious hefe-weiss beer with hints of nuts and banana',
+					inventory: 20,
+					ibu: 8,
+					abv: 5.6,
+					beerType: 'Lager',
+					beerSubType: 'Heffeweissen',
+					country: 'Germany',
+					parent_company_id: parentCompany.id
+				})
+				.expect(200)
+				.expect(res => {
+					expect(res.body).to.not.be.equal(undefined);
+					expect(res.body.name).to.be.equal('Fransiskaner');
 				}));
 	});
 });
